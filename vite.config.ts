7e1948273eaf331e1cf0c88c38a476fce42abd75
Pratik shirode <pratikshirode1234@gmail.com>
2025-07-24 23:49:25 +0530

    Updated base path and rebuilt for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/pratik_portfolio/', // ← your GitHub repo name
+  base: '/',
   plugins: [react()],
 });
